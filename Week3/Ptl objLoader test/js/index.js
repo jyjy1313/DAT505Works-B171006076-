19f676913af85850a2cae4a202e822c472cb9993
jyjy1313 <62738690+jyjy1313@users.noreply.github.com>
2020-04-08 16:54:19 +0800

    1

---
+++ Week3/Ptl objLoader test/js/index.js
@@ -0,0 +1,62 @@
+var container, stats;
+var camera, scene, raycaster, renderer;
+
+init();
+render();
+
+
+function init() {
+  container = document.createElement( 'div' );
+  document.body.appendChild( container );
+
+  camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera.position.z+=700;
+    camera.position.x-=300;
+    camera.position.y+=200;
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color( 0xf0f0f0 );
+
+  var light = new THREE.DirectionalLight( 0xffffff, 2);
+  light.position.set( 1, 1, 1 ).normalize();
+  scene.add(light);
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth/1.2, window.innerHeight/1.2);
+  container.appendChild( renderer.domElement);
+}
+/////////////////////////
+function ball1(){
+  var mtlLoader = new THREE.MTLLoader();
+  mtlLoader.load("earth.mtl", function(materials){
+  materials.preload();
+
+  var objLoader = new THREE.OBJLoader();
+  objLoader.setMaterials(materials);
+  objLoader.load("earth.obj", function(mesh){
+  mesh.scale.set(10, 10, 10);
+  scene.add(mesh);
+  });
+});
+}
+///////////////////////////////
+function ball2(){
+  var mtlLoader = new THREE.MTLLoader();
+  mtlLoader.load("moon.mtl", function(materials){
+  materials.preload();
+
+  var objLoader = new THREE.OBJLoader();
+	objLoader.setMaterials(materials);
+  objLoader.load("moon.obj", function(mesh){
+  mesh.scale.set(10, 10, 10);
+  scene.add(mesh);
+  });
+});
+}
+///////////////////////////////
+
+function render() {
+  requestAnimationFrame(render);
+  renderer.render(scene,camera);
+  document.getElementById("showball1").addEventListener("click",ball1);
+  document.getElementById("showball2").addEventListener("click",ball2);
+}
